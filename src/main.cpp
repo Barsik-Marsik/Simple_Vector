cac42e0f91bd032c5c678251c70a5dade081fc2b
Barsik-Marsik <zorinilya23@gmail.com>
2023-02-15 17:45:39 +0300

    Create the "SimpleVector<Type>" template class Create the "SimpleVector<Type>" and "ArrayPtr<Type>" template classes - Functionality of SimpleVector: 	- constructors: default, params, init list 	- deconstructor; 	- iterators: 		- begin 		- cbegin 		- end 		- cend 	- methods: 		- At 		- operator[] 		- GetSize 		- GetCapacity 		- IsEmpty 		- Clear 		- Resize
    - Functionality of ArrayPtr:
    	- constructors: default, params
    	- prohibition of copy and assignment operations
    	- swap
    	- Release
    	- Get
    	- operator[]
    
    - Tests.

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+#include "simple_vector.h"
+#include "array_ptr.h"
+// Tests
+#include "tests.h"
+
+int main() {
+    Test1();
+    std::cerr << "OK";
+    return 0;
+}
---
+++ src/simple_vector.h
@@ -0,0 +1,165 @@
+#pragma once
+
+#include <cassert>
+#include <stdexcept>
+#include <initializer_list>
+
+#include "array_ptr.h"
+
+template <typename Type>
+class SimpleVector {
+public:
+    using Iterator = Type*;
+    using ConstIterator = const Type*;
+
+    SimpleVector() noexcept = default;
+
+    // Создаёт вектор из size элементов, инициализированных значением по умолчанию
+    explicit SimpleVector(size_t size)
+        : items_(size),
+          size_(size),
+          capacity_(size)
+    {
+        std::fill(begin(), end(), Type());
+    }
+
+    // Создаёт вектор из size элементов, инициализированных значением value
+    SimpleVector(size_t size, const Type& value)
+        : items_(size),
+          size_(size),
+          capacity_(size)
+
+    {
+        std::fill(begin(), end(), value);
+    }
+
+    // Создаёт вектор из std::initializer_list
+    SimpleVector(std::initializer_list<Type> init)
+        : items_(init.size()),
+          size_(init.size()),
+          capacity_(init.size())
+        {
+            size_t i = 0;
+            for (const Type& item : init) {
+                items_[i] = item;
+                ++i;
+            }
+        }
+
+    // Возвращает количество элементов в массиве
+    size_t GetSize() const noexcept {
+        return size_;
+    }
+
+    // Возвращает вместимость массива
+    size_t GetCapacity() const noexcept {
+        return capacity_;
+    }
+
+    // Сообщает, пустой ли массив
+    bool IsEmpty() const noexcept {
+        return size_ == 0;
+    }
+
+    // Возвращает ссылку на элемент с индексом index
+    Type& operator[](size_t index) noexcept {
+        assert(index < size_);
+        return items_[index];
+    }
+
+    // Возвращает константную ссылку на элемент с индексом index
+    const Type& operator[](size_t index) const noexcept {
+        assert(index < size_);
+        const Type& const_ref = items_[index];
+        return const_ref;
+    }
+
+    // Возвращает константную ссылку на элемент с индексом index
+    // Выбрасывает исключение std::out_of_range, если index >= size
+    Type& At(size_t index) {
+        if (index >= size_) {
+            throw std::out_of_range("");
+        }
+        return items_[index];
+    }
+
+    // Возвращает константную ссылку на элемент с индексом index
+    // Выбрасывает исключение std::out_of_range, если index >= size
+    const Type& At(size_t index) const {
+        if (index >= size_) {
+            throw std::out_of_range("");
+        }
+        return items_[index];
+    }
+
+    // Обнуляет размер массива, не изменяя его вместимость
+    void Clear() noexcept {
+        size_ = 0;
+    }
+
+    // Изменяет размер массива.
+    // При увеличении размера новые элементы получают значение по умолчанию для типа Type
+    void Resize(size_t new_size) {
+        if (new_size <= size_) {
+            size_ = new_size;
+            return;
+        }
+        if (new_size < capacity_) {
+            for (size_t i = size_; i < new_size; ++i) {
+                items_[i] = Type();
+            }
+            size_ = new_size;
+            return;
+        }
+        else {
+            size_t new_capacity =  2 * new_size;
+            ArrayPtr<Type> new_items(new_capacity);
+            std::copy(begin(), end(), new_items.Get());
+            items_.swap(new_items);
+            size_ = new_size;
+            capacity_ = new_capacity;
+            return;
+        }
+    }
+
+    // Возвращает итератор на начало массива
+    // Для пустого массива может быть равен (или не равен) nullptr
+    Iterator begin() noexcept {
+        return items_.Get();
+    }
+
+    // Возвращает итератор на элемент, следующий за последним
+    // Для пустого массива может быть равен (или не равен) nullptr
+    Iterator end() noexcept {
+        return items_.Get() + size_;
+    }
+
+    // Возвращает константный итератор на начало массива
+    // Для пустого массива может быть равен (или не равен) nullptr
+    ConstIterator begin() const noexcept {
+        return items_.Get();
+    }
+
+    // Возвращает итератор на элемент, следующий за последним
+    // Для пустого массива может быть равен (или не равен) nullptr
+    ConstIterator end() const noexcept {
+        return items_.Get() + size_;
+    }
+
+    // Возвращает константный итератор на начало массива
+    // Для пустого массива может быть равен (или не равен) nullptr
+    ConstIterator cbegin() const noexcept {
+        return items_.Get();
+    }
+
+    // Возвращает итератор на элемент, следующий за последним
+    // Для пустого массива может быть равен (или не равен) nullptr
+    ConstIterator cend() const noexcept {
+        return items_.Get() + size_;
+    }
+
+private:
+    ArrayPtr<Type> items_;
+    size_t size_ = 0;
+    size_t capacity_ = 0;
+};
